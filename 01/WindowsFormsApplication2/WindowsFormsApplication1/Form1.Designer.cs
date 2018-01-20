9523a22de4473704dbe0ae9dff23de5ebac8c8ab
jash.liao <jash.liao@gmail.com>
2018-01-20 16:46:04 +0800

    Create

---
+++ 01/WindowsFormsApplication2/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DataSet1 = new WindowsFormsApplication1.DataSet1();
+            this.DataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.DataTable1BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WindowsFormsApplication1.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(284, 261);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DataSet1
+            // 
+            this.DataSet1.DataSetName = "DataSet1";
+            this.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DataTable1BindingSource
+            // 
+            this.DataTable1BindingSource.DataMember = "DataTable1";
+            this.DataTable1BindingSource.DataSource = this.DataSet1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource DataTable1BindingSource;
+        private DataSet1 DataSet1;
+    }
+}
+
---
+++ 01/WindowsFormsApplication2/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ///----指定報表外部數據源  
+            DataTable table = new DataTable();
+            table.Columns.Add("工具", typeof(string));
+            table.Columns.Add("語言", typeof(string));
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            table.Rows.Add("VS2010", "VC++ MFC");
+            table.Rows.Add("VS2010", "C# 4.0");
+            table.Rows.Add("VS2010", "VB.NET 4.0");
+            ///---添加數據源
+            ReportDataSource rds = new ReportDataSource();
+            rds.Name = "DataSet1";
+            rds.Value = table;
+            ///---向報表綁定數據源  
+            this.reportViewer1.LocalReport.DataSources.Clear();//注，使用原文代碼上測試，數據一直不顯示，後來添加了這句代碼，代碼出現了。MS真坑。
+            this.reportViewer1.LocalReport.DataSources.Add(rds);
+            //this.reportViewer1.LocalReport.ReportPath = @"..\..\Report1.rdlc";
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
